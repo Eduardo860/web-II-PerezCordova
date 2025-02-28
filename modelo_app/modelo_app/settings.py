be1d3b82878611c0c9d66528213a559ca3a71556
Eduardo860 <ep321889@gmail.com>
2025-02-27 23:50:02 -0600

    Inicializacion Examen

--- modelo_app/modelo_app/settings.py
+++ modelo_app/modelo_app/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orders.apps.OrdersConfig',
-    'users.apps.UsersConfig',
+    
 ]
 
 MIDDLEWARE = [
