b780d27baa75fdebe70d24e271e0f51d2600dc81
Eduardo860 <ep321889@gmail.com>
2025-04-26 21:05:55 -0600

    Proyecto 2 Finalizado

--- proyecto2/src/App.js
+++ proyecto2/src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    
-      </header>
+        <Home/>
     </div>
   );
 }
